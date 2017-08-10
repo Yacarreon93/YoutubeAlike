7711b3e100e5af3f9b35a300ab4ded95f4e60a58
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-08-10 16:45:32 -0500

    Include select video function

--- src/index.js
+++ src/index.js
@@ -15,10 +15,14 @@ class App extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null
         }
         YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ videos })
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         })
     }
 
@@ -26,8 +30,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
             </div>
         )
     }
