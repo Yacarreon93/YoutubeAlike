cec3e8c1ac0d89633f4915a13f63ed4668ed674e
Yasser Arturo Carreon Lopez <yasser carreon@tiempodev.local>
2017-08-10 17:00:56 -0500

    Include search video function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,8 +10,9 @@ class SearchBar extends Component {
         this.onInputChange = this.onInputChange.bind(this)
     }
 
-    onInputChange(event) {        
-        this.setState({ term: event.target.value})
+    onInputChange(term) {        
+        this.setState({ term })
+        this.props.onSearchTermChange({ term })
     }
 
     render () {
@@ -19,7 +20,7 @@ class SearchBar extends Component {
             <div className="search-bar">
                 <input 
                     value={this.state.term} 
-                    onChange={this.onInputChange} />
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         )
     }
